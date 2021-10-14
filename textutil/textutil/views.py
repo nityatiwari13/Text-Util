f5be955607db5f7f41b2d2690602f90f102e0931
Nitya <nitya4045@gmail.com>
2021-10-14 22:00:16 +0530

    commit

--- textutil/textutil/views.py
+++ textutil/textutil/views.py
@@ -34,8 +34,8 @@ def analyze(request):
             if(char != " "):
                 cnt = cnt + 1
         analyser = str(cnt)     
-        if (countdigit == 'on'):
-            cn = 0
+    if (countdigit == 'on'):
+        cn = 0
         for char in txt:
             if (char=='0' or char=='1' or char=='2' or char=='3' or char=='4' or char=='5' or char=='6' or char=='7' or char=='8' or char=='9'):
                 cn = cn + 1           
